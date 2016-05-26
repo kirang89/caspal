cb5ab04beb91a4f7c68be2ef224a0e803b9f822b
kirang89 <me@kirang.in>
2016-05-26 13:12:06 +0530

    Modify tests to make them pass

--- tests/test_caspal.py
+++ tests/test_caspal.py
@@ -21,49 +21,49 @@ class CaspalTestCase(unittest.TestCase):
         self.assertEqual(
             evaluate('PROGRAM example; Var a : integer; BEGIN a := 1+4-2 END.'), None
         )
-        self.assertEqual(ENVIRONMENT['a'], 3)
+        self.assertEqual(ENVIRONMENT['a'][0], 3)
 
         self.assertEqual(
             evaluate('PROGRAM example; Var b: integer; BEGIN b := 1+4*2 END.'), None
         )
-        self.assertEqual(ENVIRONMENT['b'], 9)
+        self.assertEqual(ENVIRONMENT['b'][0], 9)
 
         self.assertEqual(
             evaluate('PROGRAM example; Var foo:integer; BEGIN foo := 1+4/2 END.'), None
         )
-        self.assertEqual(ENVIRONMENT['foo'], 3.0)
+        self.assertEqual(ENVIRONMENT['foo'][0], 3.0)
 
         self.assertEqual(
             evaluate('PROGRAM example; Var bar:integer; BEGIN bar := 1*4/2 END.'), None
         )
-        self.assertEqual(ENVIRONMENT['bar'], 2)
+        self.assertEqual(ENVIRONMENT['bar'][0], 2)
 
     def test_arith_expr_with_user_precedence(self):
         self.assertEqual(
             evaluate('PROGRAM example; Var a:integer; BEGIN a := 2*(10/(2+3)) END.'), None
         )
-        self.assertEqual(ENVIRONMENT['a'], 4.0)
+        self.assertEqual(ENVIRONMENT['a'][0], 4.0)
 
         self.assertEqual(
             evaluate('PROGRAM example; Var b:integer; BEGIN b := 2*(2+3) END.'), None
         )
-        self.assertEqual(ENVIRONMENT['b'], 10)
+        self.assertEqual(ENVIRONMENT['b'][0], 10)
 
     def test_multiple_statements(self):
         self.assertEqual(
             evaluate('PROGRAM example; Var a:integer; BEGIN a := 2*(10/(2+3)); b := 4 END.'),
             None
         )
-        self.assertEqual(ENVIRONMENT['a'], 4.0)
-        self.assertEqual(ENVIRONMENT['b'], 4)
+        self.assertEqual(ENVIRONMENT['a'][0], 4.0)
+        self.assertEqual(ENVIRONMENT['b'][0], 4)
 
         self.assertEqual(
             evaluate('PROGRAM example; Var b, d:integer; BEGIN b := 2*(2+3); c := 1 + 3; d := 12 END.'),  # noqa
             None
         )
-        self.assertEqual(ENVIRONMENT['b'], 10)
-        self.assertEqual(ENVIRONMENT['c'], 4)
-        self.assertEqual(ENVIRONMENT['d'], 12)
+        self.assertEqual(ENVIRONMENT['b'][0], 10)
+        self.assertEqual(ENVIRONMENT['c'][0], 4)
+        self.assertEqual(ENVIRONMENT['d'][0], 12)
 
     def test_declarations(self):
         self.assertEqual(
@@ -71,9 +71,9 @@ class CaspalTestCase(unittest.TestCase):
                 'PROGRAM example; Var a,b,c:integer; BEGIN a := 2; b := 3; c := 4 END.'
             ), None)
 
-        self.assertEqual(ENVIRONMENT['b'], 3)
-        self.assertEqual(ENVIRONMENT['c'], 4)
-        self.assertEqual(ENVIRONMENT['a'], 2)
+        self.assertEqual(ENVIRONMENT['b'][0], 3)
+        self.assertEqual(ENVIRONMENT['c'][0], 4)
+        self.assertEqual(ENVIRONMENT['a'][0], 2)
 
 
 if __name__ == '__main__':
